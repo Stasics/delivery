1cb0ee829bb997f6ec02cb6fa9e0818c4cd5ef66
Anasteisha <anasteisha.s333@yandex.ru@whatever.com>
2025-06-05 15:01:30 +0300

    render

--- main.py
+++ main.py
@@ -1,21 +1,13 @@
+from fastapi import FastAPI
 import uvicorn
-from app.core.config import settings
-import os  # Import the 'os' module
+import os
 
-# from app import app # Убрали, так как теперь используем import string
-# from app.models import Base, engine # Импорт engine
+app = FastAPI()
 
-# app = create_app() # убрали, app уже инициализировано
-
-# async def create_db_and_tables():
-#     async with engine.begin() as conn:
-#         await conn.run_sync(Base.metadata.create_all)
+@app.get("/")  # <--- This defines a route for the root path (/)
+async def read_root():
+    return {"message": "Hello, World! from FastAPI on Render"} # or a JSON message
 
 if __name__ == "__main__":
-    # import asyncio
-    # asyncio.run(create_db_and_tables())
-
-    # Use Render's PORT environment variable, or default to 8000 if not set
-    port = int(os.environ.get("PORT", 8000))  # <----  IMPORTANT
-
-    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)  # <----  Use the variable 'port' and include reload as you wish
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("app:app", host="0.0.0.0", port=port, reload=True)
