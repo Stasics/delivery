57a6971e39502a594aef8c2c7f5554ccfdd58eb3
Anasteisha <anasteisha.s333@yandex.ru@whatever.com>
2025-06-05 16:05:02 +0300

    last CORS

--- app/__init__.py
+++ app/__init__.py
@@ -7,9 +7,10 @@ app = FastAPI()
 # Настройка CORS
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://project-delivery-roan.vercel.app/"],  # Адрес Live Server
-    allow_methods=["*"],
-    allow_headers=["*"],
+    allow_origins=["https://project-delivery-roan.vercel.app"],  # URL вашего фронтенда
+    allow_methods=["*"],  # Разрешить все методы (GET, POST, OPTIONS и т. д.)
+    allow_headers=["*"],  # Разрешить все заголовки
+    allow_credentials=True,  # Если используются куки или авторизация
 )
 
 app.include_router(user_routes.router)
